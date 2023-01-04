010020a79e9d30e81f4696e18f64ce6dce5d15a3
Waldo3333 <117690497+Waldo3333@users.noreply.github.com>
2023-01-05 00:10:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function goStepTwo() {
-  stepInfo.style.display = "none";
-  stepPlan.style.display = "flex";
-  circle1.style.color = "white";
-  circle1.style.backgroundColor = "rgba(255, 255, 255, 0)";
-  circle2.style.backgroundColor = "rgb(255, 255, 255)";
-  circle2.style.color = "black";
+  checkName();
+  checkNum();
+  checkMail();
+  Achieve();
 }
 
 function backStepOne() {
@@ -17,13 +15,6 @@ function backStepOne() {
 }
 
 function goStepThree() {
-  stepPlan.style.display = "none";
-  stepAddOn.style.display = "flex";
-  circle2.style.color = "white";
-  circle2.style.backgroundColor = "rgba(255, 255, 255, 0)";
-  circle3.style.backgroundColor = "rgb(255, 255, 255)";
-  circle3.style.color = "black";
-
   console.log(typeof document.getElementById("totalPrice").innerHTML);
 
   if (document.getElementById("totalPrice").innerHTML == "") {
@@ -31,6 +22,7 @@ function goStepThree() {
     document.getElementById("totalPrice").innerHTML = "0";
     console.log(document.getElementById("totalPrice").innerHTML);
   }
+  checkPlan();
 }
 function backSteptTwo() {
   stepPlan.style.display = "flex";
@@ -101,10 +93,10 @@ function goFromFourToTwo() {
   });
 }
 function goToStepThankYou() {
-  if (document.getElementById("modeResume").innerHTML == "Select a plan") {
+  console.log(document.getElementById("modeResume").innerHTML);
+  if (document.getElementById("modeResume").innerHTML == "Choose a plan") {
     stepSummary.style.display = "flex";
     stepThankYou.style.display = "none";
-    alert("Select A Plan");
   } else {
     stepSummary.style.display = "none";
     stepThankYou.style.display = "flex";
@@ -382,4 +374,92 @@ function getPrice6() {
   const planPrice = "150";
   console.log(planPrice);
   document.getElementById("totalPrice").innerHTML = planPrice;
+}
+
+/* check des inputs INFOS */
+
+function checkName() {
+  let myNameInput = document.getElementById("infoName");
+  let myNameError = document.getElementById("errorName");
+  let myNameRegex = /^[a-zA-Z-\s]+$/;
+
+  if (myNameInput.value.trim() == "") {
+    myNameError.innerHTML = "This field is required";
+  } else if (myNameRegex.test(myNameInput.value) == false) {
+    myNameError.innerHTML = "Can't contain numbers or symbols";
+  } else if (myNameInput.value.trim() !== "") {
+    myNameError.innerHTML = "";
+  }
+}
+
+function checkNum() {
+  let myNumInput = document.getElementById("infoNumber");
+  let myNumError = document.getElementById("errorNum");
+  let myNumRegex = /^[0-9\s]+$/;
+
+  if (myNumInput.value.trim() == "") {
+    myNumError.innerHTML = "This field is required";
+  } else if (myNumRegex.test(myNumInput.value) == false) {
+    myNumError.innerHTML = "Must contain number";
+  } else if (myNumInput.value.trim().length !== 10) {
+    myNumError.innerHTML = "Invalid Phone Number";
+  } else {
+    myNumError.innerHTML = "";
+  }
+}
+
+function checkMail() {
+  let myMailInput = document.getElementById("infoMail");
+  let myNumError = document.getElementById("errorMail");
+  let myMailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (myMailInput.value.trim() == "") {
+    myNumError.innerHTML = "This field is required";
+  } else if (myMailRegex.test(myMailInput.value) == false) {
+    myNumError.innerHTML = "Please enter a valid e-mail";
+  } else {
+    myNumError.innerHTML = "";
+  }
+}
+
+function Achieve() {
+  let myNumInput = document.getElementById("infoNumber");
+  let myNameInput = document.getElementById("infoName");
+  let myMailInput = document.getElementById("infoMail");
+  let myNameRegex = /^[a-zA-Z-\s]+$/;
+  let myNumRegex = /^[0-9\s]+$/;
+  let myMailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (
+    myNumInput.value.trim().length === 10 &&
+    myNumRegex.test(myNumInput.value) == true &&
+    myMailInput.value.trim().length !== 0 &&
+    myMailRegex.test(myMailInput.value) === true &&
+    myNameInput.value.trim().length !== 0 &&
+    myNameRegex.test(myNameInput.value) === true
+  ) {
+    stepInfo.style.display = "none";
+    stepPlan.style.display = "flex";
+    circle1.style.color = "white";
+    circle1.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    circle2.style.backgroundColor = "rgb(255, 255, 255)";
+    circle2.style.color = "black";
+  }
+}
+function checkPlan() {
+  if (document.getElementById("modeResume").innerHTML == "Choose a plan") {
+    stepInfo.style.display = "none";
+    stepPlan.style.display = "flex";
+    circle1.style.color = "white";
+    circle1.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    circle2.style.backgroundColor = "rgb(255, 255, 255)";
+    circle2.style.color = "black";
+  } else {
+    stepPlan.style.display = "none";
+    stepAddOn.style.display = "flex";
+    circle2.style.color = "white";
+    circle2.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    circle3.style.backgroundColor = "rgb(255, 255, 255)";
+    circle3.style.color = "black";
+  }
 }
